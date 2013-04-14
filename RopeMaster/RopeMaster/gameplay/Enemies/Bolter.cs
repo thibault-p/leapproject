d2424e5bce291f23ac9db2f0966f586444288a28
thibault <thibaultperson@gmail.com>
2013-04-14 23:26:06 +0200

    Stuff

---
+++ RopeMaster/RopeMaster/gameplay/Enemies/Bolter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RopeMaster.gameplay.Enemies
+{
+    public class Bolter
+    {
+        private List<Bolt> boltList;
+
+
+
+        public Bolter()
+        {
+            boltList = new List<Bolt>(10);
+
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+
+
+
+
+        class  Bolt{
+
+            private List<>
+
+            public Bolt(Vector2 start , Vector2 end){
+
+            }
+
+
+
+
+        }
+
+        class Segment{
+
+
+
+
+
+        }
+    }
+
+}
